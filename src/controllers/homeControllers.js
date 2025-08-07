d3113feefd456725b5668fa23a5319fb1dbabd65
nini <bploveme048@gmail.com>
2025-08-07 22:32:37 +0700

    navbar

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,11 +1,17 @@
+const connection = require('../config/database')
 const getHomePage = (req, res) => {
-     res.send('Hello World & nini')
+    return res.render('home.ejs')
+
 }
 const getABC = (req, res) => {
+
+
      res.send('Hello World & abc')
+
 }
 const getNini = (req, res) => {
-      res.render('sample.ejs')
+     res.render('sample.ejs')
+     
 }
 
-module.exports = {getHomePage, getABC, getNini}
+module.exports = { getHomePage, getABC, getNini }
--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express');
 const configViewEngine = require('./config/viewEngine')
 const webRouters = require('./routes/web')
-const mysql = require('mysql2')
+const connection = require('./config/database')
+
 const app = express();
 const port = process.env.PORT || 8081;
 const hostname = process.env.HOST_NAME;
@@ -10,20 +11,10 @@ const hostname = process.env.HOST_NAME;
 configViewEngine(app)
 app.use('/', webRouters)
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '123456',
-    database: 'hoidanit',
-
-})
 connection.query(
     'select * from Users u',
     function (err, results, fields) {
-        console.log('>>result',results);
-        console.log('>>>>fields',fields);
-
+        console.log('>>result', results);
     }
 )
 
